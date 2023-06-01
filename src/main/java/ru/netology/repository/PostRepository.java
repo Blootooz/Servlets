accb5c6e48096e773e05fce21a17b214eb5fc9d5
Alexandra <84934027+Blootooz@users.noreply.github.com>
2023-06-01 21:39:21 +0300

    create branch feature/di-annotation

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.model.Post;
 
 import java.util.Collection;
@@ -8,6 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
 
+@Repository
 public class PostRepository {
   private final ConcurrentMap<Long, Post> allPosts;
   private final AtomicLong idCounter = new AtomicLong();
@@ -16,13 +18,13 @@ public class PostRepository {
     this.allPosts = new ConcurrentHashMap<>();
   }
 
-    public Collection<Post> all() {
-      return allPosts.values();
-    }
+  public Collection<Post> all() {
+    return allPosts.values();
+  }
 
-    public Optional<Post> getById(long id) {
-      return Optional.ofNullable(allPosts.get(id));
-    }
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(allPosts.get(id));
+  }
 
   public Post save(Post savePost) {
     if (savePost.getId() == 0) {
